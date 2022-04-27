dc6a0b0f222fe534040247d7afc9a23922e7743f
LoboAnimae <andyquan12@gmail.com>
2022-04-27 13:25:08 -0600

    Changed to node because of sorting implementations

--- src/ErrorTypes.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Yagdrassyl on 23/04/22.
-//
-
-#include "ErrorTypes.hpp"
-
-namespace Error {
-} // Error
--- src/Menu.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Yagdrassyl on 23/04/22.
-//
-
-#include "Menu.hpp"
-
-namespace Menu {
-} // Menu
